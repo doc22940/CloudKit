6927fe4e0f41f7b9ade96d9f581bb97ebf8b1240
Swapnanil Dhol <swapnanildhol@gmail.com>
2019-12-18 15:07:40 +0530

    Initial Commit

---
+++ CloudKit/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  CloudKit
+//
+//  Created by Swapnanil Dhol on 12/18/19.
+//  Copyright © 2019 Swapnanil Dhol. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
